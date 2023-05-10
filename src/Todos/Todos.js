199137e51383adacb26dec896f104e790581147f
Celeste A. Moore <77650238+celestealexmoore@users.noreply.github.com>
2023-05-10 16:22:47 -0500

    styled tasks

--- src/Todos/Todos.js
+++ src/Todos/Todos.js
@@ -1,10 +1,21 @@
 import "./Todos.css";
+import { useState } from "react";
 
 function Todos() {
+  let [checked, setChecked] = useState(false);
+
+  function isChecked() {
+    !checked ? setChecked(true) : setChecked(false);
+  }
+
   return (
-    <>
-      <div className="todosParent">hello world </div>
-    </>
+    <div className="todosParent">
+      <div className="indTask">
+        <input type="checkbox" className="checkbox" onClick={isChecked} />
+        <p className={`${checked ? "completed" : ""}`}>Task One</p>
+        <button className="deleteBtn">Delete</button>
+      </div>
+    </div>
   );
 }
 
