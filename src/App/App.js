1f965378ed06037e1b85fbc00192c6b47ba388ca
Celeste A. Moore <77650238+celestealexmoore@users.noreply.github.com>
2023-05-10 23:09:12 -0500

    adding tasks to array

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,43 @@
 import "./App.css";
+import { useState } from "react";
 import Todos from "../Todos/Todos.js";
 
 function App() {
+  let taskArr = [];
+  let userInput;
+
+  // let [input, setInput] = useState("");
+
+  function updateInput(e) {
+    userInput = e.target.value;
+    // setInput(e.target.value);
+  }
+
+  function addTask() {
+    console.log(userInput);
+    taskArr.push(userInput);
+    console.log(taskArr);
+    // setInput("");
+  }
+
   return (
     <>
       <div className="app">
         <h1 className="title">todos</h1>
 
         <div className="inputParent">
-          <input type="text" className="input" placeholder="Add Todo..." />
-          <button type="submit" className="inputBtn">
+          <input
+            type="text"
+            className="input"
+            placeholder="Add Todo..."
+            onChange={updateInput}
+          />
+          <button type="submit" className="inputBtn" onClick={addTask}>
             Submit
           </button>
         </div>
 
-        <Todos />
+        <Todos taskArr={taskArr} />
       </div>
     </>
   );
--- src/Todos/Todos.js
+++ src/Todos/Todos.js
@@ -1,20 +1,30 @@
 import "./Todos.css";
 import { useState } from "react";
 
-function Todos() {
-  let [checked, setChecked] = useState(false);
+function Todos(props) {
+  const [checked, setChecked] = useState(false);
 
   function isChecked() {
     !checked ? setChecked(true) : setChecked(false);
   }
 
+   /* 
+  Store what is in taskArr to localStorage.
+  Then I want to map over the taskArr.
+  For each task, create a todo div element in the child component.
+  */
+
   return (
     <div className="todosParent">
-      <div className="indTask">
-        <input type="checkbox" className="checkbox" onClick={isChecked} />
-        <p className={`${checked ? "completed" : ""}`}>Task One</p>
-        <button className="deleteBtn">Delete</button>
-      </div>
+      {props.taskArr.map((task) => {
+        return (
+          <div className="indTask">
+            <input type="checkbox" className="checkbox" onClick={isChecked} />
+            <p className={`${checked ? "completed" : ""}`}>{task}</p>
+            <button className="deleteBtn">Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
